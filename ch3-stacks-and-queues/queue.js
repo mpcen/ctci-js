6312a0d858b2d2a8177dad3b4c563825deb60219
mpcen <vccengineers12@gmail.com>
2018-02-02 23:31:28 -0800

    added queue

---
+++ ch3-stacks-and-queues/queue.js
@@ -0,0 +1,63 @@
+/*
+	Queue Implementation
+	FIFO - First in first out
+	Methods:
+	- add - Adds an item to the end of the list
+	- remove - Removes the first item out of the list
+	- peek - Returns the first item of the list
+	- isEmpty - Returns true if the list is empty
+*/
+
+class QueueNode {
+	constructor(data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+class Queue {
+	constructor(data) {
+		const newItem = new QueueNode(data);
+
+		this.front = newItem;
+		this.last = newItem;
+	}
+
+	add(data) {
+		const newItem = new QueueNode(data);
+
+		if(!this.first) {
+			this.first = newItem;
+		}
+		
+		if(this.last) {
+			this.last.next = newItem;
+		}
+
+		this.last = newItem;
+	}
+
+	remove() {
+		if(!this.first) return null;
+
+		const item = this.fist;
+
+		this.first = item.next;
+
+		if(!this.first) {
+			this.last = null;
+		}
+
+		return item;
+	}
+
+	peek() {
+		if(!this.first) return null;
+
+		return this.first;
+	}
+
+	isEmpty() {
+		return this.first === null;
+	}
+}
