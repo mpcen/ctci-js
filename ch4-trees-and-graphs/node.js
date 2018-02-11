4794e04928d92f659f79fbf2aa627d64b2dfdf10
mpcen <vccengineers12@gmail.com>
2018-02-10 19:20:35 -0800

    added node, contains, traverseDF to bst

---
+++ ch4-trees-and-graphs/node.js
@@ -0,0 +1,55 @@
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+
+	insert(data) {
+		if(data < this.data && !this.left) {
+			this.left = new Node(data);
+		} else if(data < this.data && this.left) {
+			this.left.insert(data);
+		} else if (data > this.data && !this.right) {
+			this.right = new Node(data);
+		} else {
+			this.right.insert(data);
+		}
+	}
+
+	contains(data) {
+		if(data === this.data) {
+			return this;
+		}
+
+		if(data < this.data && this.left) {
+			return this.left.contains(data);
+		} else if(data > this.data && this.right) {
+			return this.right.contains(data);
+		} else {
+			return null;
+		}
+	}
+
+	traverseDF(fn, order) {
+		if(order === 'pre-order') {
+			fn(this);
+		}
+
+		if(this.left) {
+			this.left.traverseDF(fn, order);
+		}
+
+		if(order === 'in-order') {
+			fn(this);
+		}
+
+		if(this.right) {
+			this.right.traverseDF(fn, order);
+		}
+
+		if(order === 'post-order') {
+			fn(this);
+		}
+	}
+}
