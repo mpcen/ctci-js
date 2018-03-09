01f66d2a41d4bf86bdc28cf780568503e974c413
mpcen <vccengineers12@gmail.com>
2018-03-08 21:36:28 -0800

    added stringPermutation

--- ch-08-recursion-and-dynamic-programming/stringPermutation.js
+++ ch-08-recursion-and-dynamic-programming/stringPermutation.js
@@ -1,9 +1,41 @@
-const permute = (string, p = '') => {
-	if(!string.length) {
+/*
+
+Given a string s, print all permutations of that string
+
+*/
+
+const permute = (s, p = '') => {
+	if(!s.length) {
 		console.log(p);
 	} else {
-		for(let i = 0; i < string.length; i++) {
-			permute(string.substring(0, i) + string.substring(i + 1, string.length), p + string.charAt(i))
+		for(let i = 0; i < s.length; i++) {
+			let c = s.charAt(i);
+
+			s = s.split('');
+			s.splice(i, 1);
+			s = s.join('');
+
+			p += c;
+
+			permute(s, p);
+
+			s = s.split('');
+			s.splice(i, 0, c);
+			s = s.join('');
+
+			p = p.split('');
+			p.splice(p.length - 1, 1);
+			p = p.join('');
 		}
 	}
 }
+
+
+/*
+many|
+any|m
+ny|ma
+y|man
+|many
+
+*/
